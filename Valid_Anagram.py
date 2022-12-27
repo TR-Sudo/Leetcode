45bb950bdcce9cc8857b3d8d55328e29f0861ce8
Talha Rashid <T.rashid489@gmail.com>
2022-12-26 19:30:06 -0500

    Solution to Valid Anagram O(S+T)

---
+++ Valid_Anagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        smap,tmap={},{}
+        for s,val in enumerate(s):
+            smap[val]=smap.get(val,0)+1
+
+        for t,val in enumerate(t):
+            tmap[val]=tmap.get(val,0)+1
+        return tmap==smap 
