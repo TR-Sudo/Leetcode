4b01cf837bd5b7baa57f6d31fe842c1ae1e914ed
TR-Sudo <78048789+TR-Sudo@users.noreply.github.com>
2023-03-06 15:35:42 -0500

    File Naming Scheme

--- Contains Duplicate/contains_duplicate.py
+++ Contains Duplicate/contains_duplicate.py
@@ -1,13 +1,1 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        #Solution using a hash set O(N)
-        hash=set()
-        for x,val in enumerate(nums):
-            if val in hash:
-                return True
-            hash.add(val)
         return False
